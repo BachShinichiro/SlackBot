02c64a4a38349f3a7eaee064394167c838de0472
BachShinichiroe <kitchen.number@gmail.com>
2020-10-09 14:17:10 +0900

    [first]commit

---
+++ sample.rb
@@ -0,0 +1,16 @@
+require "slack-notify"
+require 'clockwork'
+require 'active_support/time'
+
+ #APIは保守の観点から記載しておりません
+sample = SlackNotify::Client.new(webhook_url:'API')
+sample.notify("Hello test!")
+
+module Clockwork
+    handler do |job|
+        #APIは保守の観点から記載しておりません
+        sample = SlackNotify::Client.new(webhook_url:'API')
+        sample.notify("バッハbot参上")
+    end
+    every(10.seconds, 'less.frequent.job')
+end
